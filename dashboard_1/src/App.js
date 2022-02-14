213450e8928df854a7ecf6f85c25d44c916939ad
FrancisTocino <francistocino@hotmail.com>
2022-02-14 19:09:48 +0100

    First  Commit Whit squeleton

---
+++ dashboard_1/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div class="container">
+        <div class="sidebar">
+         <p>Hola Que Tal</p>
+        </div>
+        <div class="main-content">
+          <p>Soy elContainer</p>
+        </div>
+    </div>
+  </>
+  );
+}
+
+export default App;
